ffbb9e13640f4dcbce071ae92595a0131025c47c
ministicraft <worldtocraft@gmail.com>
2018-04-16 22:47:16 +0200

    load datatables data by ajax call

--- public/js/librarys/librarys.js
+++ public/js/librarys/librarys.js
@@ -1,3 +1,30 @@
-$(document).ready(function() {
-    $('input#search').characterCounter();
+var elem = document.querySelector('.sidenav');
+var instance = M.Sidenav.init(elem, {
+    dragable: true
+});
+var dataTable = new DataTable("#myTable", {
+    searchable: true,
+    perPage: 10,
+    ajax: "/data.json",
+    columns: [
+        {
+            select: 0,
+            sortable: false,
+            render: function(data,cell,row){
+                cell.classList.add("col");
+                cell.classList.add("s1");
+                cell.classList.add("artwork");
+                row.classList.add("row");
+                return "<img src=\"data:image/jpeg;base64,"+data+"\">";
+            }
+        },
+        {
+            select: 1,
+            render: function (data, cell, row) {
+                cell.classList.add("col");
+                cell.classList.add("s11");
+                return data;
+            }
+        }
+    ]
 });
